d74949b1c4ce028c769c2dd41abbcea6afc317fd
Sebastian.Abele <sebastian.abele@wolterskluwer.com>
2018-05-02 19:59:42 +0200

    bla

--- views/ss_available/index.js
+++ views/ss_available/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, View, Button, TextInput, Picker, Input, Alert } from 'react-native';
+import DatePicker from 'react-native-datepicker'
 import config  from '../../helper/config';
 import tokenHelper from '../../helper/token';
 
@@ -71,7 +72,31 @@ export default class SocialSecurity extends Component {
               );
             })}
           </Picker>
-          : null}     
+          : null} 
+          <DatePicker
+            style={{width: 200}}
+            date={this.state.date}
+            mode="date"
+            placeholder="select date"
+            format="YYYY-MM-DD"
+            minDate="2018-05-01"
+            maxDate="2018-07-01"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+              // ... You can check the source to find the other keys.
+            }}
+            onDateChange={(date) => {this.setState({date: date})}}
+          />
         <Button
           onPress={this._register.bind(this)}
           title="Send Data"
